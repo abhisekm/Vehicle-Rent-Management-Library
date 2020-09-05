79741951bb44038ff96e196540bc93c3c145dd82
abhisekm <abhisekmajumder07@gmail.com>
2020-09-05 13:35:40 +0530

    update demo class

--- VehicleRentManagementLibrary/src/main/java/com/abhisekm/vehiclerentmanagementlibrary/Demo.kt
+++ VehicleRentManagementLibrary/src/main/java/com/abhisekm/vehiclerentmanagementlibrary/Demo.kt
@@ -7,8 +7,10 @@ import android.widget.Toast
  * Demo class to test the library module
  */
 
-object Demo {
-    fun runDemo(context: Context){
-        Toast.makeText(context,"Demo Library Setup",Toast.LENGTH_LONG).show()
+class Demo {
+    companion object{
+       fun runDemo(context: Context){
+           Toast.makeText(context,"Demo Library Setup",Toast.LENGTH_LONG).show()
+       }
     }
 }
