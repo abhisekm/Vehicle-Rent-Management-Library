79669f3098bb533ea7399ac3377ca9f7ca0fc214
abhisekm <abhisekmajumder07@gmail.com>
2020-09-05 12:49:11 +0530

    initial commit

---
+++ VehicleRentManagementLibrary/src/main/java/com/abhisekm/vehiclerentmanagementlibrary/Demo.kt
@@ -0,0 +1,14 @@
+package com.abhisekm.vehiclerentmanagementlibrary
+
+import android.content.Context
+import android.widget.Toast
+
+/**
+ * Demo class to test the library module
+ */
+
+object Demo {
+    fun runDemo(context: Context){
+        Toast.makeText(context,"Demo Library Setup",Toast.LENGTH_LONG).show()
+    }
+}
